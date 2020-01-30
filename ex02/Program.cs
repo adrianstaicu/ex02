c3c6f1d27c8aac2b05794d97c2f3f784e816f6e7
Staicu Adrian <adrianstacu888@gmail.com>
2020-01-31 00:19:34 +0200

    Add project files.

---
+++ ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double d = 2.95;
+            int i = 4;
+            Console.WriteLine(++d > i ? d : i);
+            Console.Read();
+            //  Putem spune ca avem de aface cu comparatia a doua tipuri de date aritmetice si o bluca if else
+            // Afiseaza 4 deoarece prima conditie nu este adevarata si nu se executa
+        }
+    }
+}
